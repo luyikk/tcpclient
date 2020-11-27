f2a70a8edf4a0204fb28163a54a7d133beed7d45
yi lu <luyikk@126.com>
2020-11-27 15:31:19 +0800

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,106 @@
+#![feature(async_closure)]
+use tokio::net::{TcpStream, ToSocketAddrs};
+use std::io;
+use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
+use tokio::io::{AsyncWriteExt, ErrorKind};
+use tokio::macros::support::Future;
+use aqueue::Actor;
+use std::ops::Deref;
+use std::sync::Arc;
+use std::net::Shutdown;
+use log::*;
+use std::error::Error;
+use aqueue::AError::{Other, StrErr};
+
+pub struct TcpClient {
+    disconnect:bool,
+    sender:OwnedWriteHalf
+}
+
+
+impl TcpClient {
+    pub async fn connect<T:ToSocketAddrs,F:Future<Output=Result<bool,Box<dyn Error>>>+Send+'static>(addr:T, f:impl FnOnce(Arc<Actor<TcpClient>>,OwnedReadHalf)->F+Send+'static) ->io::Result<Arc<Actor<TcpClient>>>{
+
+        let stream= TcpStream::connect(addr).await?;
+        let target= stream.peer_addr()?;
+        let(reader,sender)= stream.into_split();
+        let client=Arc::new(Actor::new(TcpClient {
+            disconnect:false,
+            sender
+        }));
+
+        let read_client=client.clone();
+        tokio::spawn(async move{
+            let disconnect_client=read_client.clone();
+            let need_disconnect=
+                match f(read_client,reader).await{
+                    Ok(disconnect)=>{
+                        disconnect
+                    },
+                    Err(err)=>{
+                        error!("reader error:{}",err);
+                        true
+                    }
+                };
+
+            if need_disconnect {
+                if let Err(er)= disconnect_client.disconnect().await{
+                    error!("disconnect to{} err:{}",target,er);
+                }
+                else{
+                    debug!("disconnect to {}",target)
+                }
+            }
+            else{
+                debug!("{} reader is close",target);
+            }
+        });
+        Ok(client)
+    }
+
+    pub fn disconnect(&mut self)->io::Result<()>{
+        if !self.disconnect {
+            self.sender.as_ref().shutdown(Shutdown::Both)?;
+            self.disconnect = true;
+        }
+        Ok(())
+    }
+
+    pub async fn send(&mut self, buff:&[u8])->io::Result<usize>{
+        if !self.disconnect {
+            self.sender.write(buff).await
+        }else{
+            Err(io::Error::new(ErrorKind::Other,StrErr("Send Error,Disconnect".into())))
+        }
+    }
+
+
+}
+
+#[aqueue::aqueue_trait]
+pub trait SocketClientTrait{
+    async fn send<T:Deref<Target=[u8]>+Send+Sync+'static>(&self,buff:T)->Result<usize,Box<dyn Error>>;
+    async fn disconnect(&self)->Result<(),Box<dyn Error>>;
+}
+
+#[aqueue::aqueue_trait]
+impl SocketClientTrait for Actor<TcpClient>{
+    async fn send<T:Deref<Target=[u8]>+Send+Sync+'static>(&self, buff:T)->Result<usize,Box<dyn Error>>{
+        Ok(self.inner_call(async move |inner|{
+            match inner.get_mut().send(&buff).await {
+                Ok(size)=>Ok(size),
+                Err(er)=>Err(Other(er.into()))
+            }
+        }).await?)
+    }
+
+    async fn disconnect(&self) ->Result<(),Box<dyn Error>> {
+        self.inner_call(async move |inner| {
+            match inner.get_mut().disconnect() {
+                Ok(_) => Ok(()),
+                Err(er) => Err(Other(er.into()))
+            }
+        }).await?;
+        Ok(())
+    }
+}
